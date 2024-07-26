24f7becf46a648c7ee1e4156e33e42b529694ce8
Rupam <152799226+CodeExplorer4267@users.noreply.github.com>
2024-07-26 17:30:12 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB= async ()=>{
    try {
-     const connectionInstance=await mongoose.connect(`mongodb+srv://rupambhadra478:rupam478@cluster0.w9few8s.mongodb.net/${DB_NAME}`)
+     const connectionInstance=await mongoose.connect(`url/${DB_NAME}`)
      console.log(`\n MongoDB connected !! DB Host:${connectionInstance.connection.host}`)
    } catch (error) {
      console.log("Error is:",error);
